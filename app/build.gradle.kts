547b28383ab623e3532e87513ea85c551766e31d
Sindre Moen <sindrenm@gmail.com>
2021-10-29 15:49:35 +0200

    Bump version 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     minSdk = BuildValues.minSdkVersion
     targetSdk = BuildValues.targetSdkVersion
 
-    versionCode = 2
-    versionName = "0.2.0"
+    versionCode = 3
+    versionName = "0.3.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
