b6965b4d3a99ee88d31a8da5610ddc087330b8e4
Sindre Moen <sindrenm@gmail.com>
2021-11-11 21:51:25 +0100

    Remove unused imports

--- programs/src/main/java/dev/muskelmekka/cannons/programs/ProgramsScreen.kt
+++ programs/src/main/java/dev/muskelmekka/cannons/programs/ProgramsScreen.kt
@@ -23,10 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
